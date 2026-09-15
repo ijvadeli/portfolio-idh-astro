784850f30871106cfbb89c8c0ecca26cbd557592
ijvadeli <idhvandelindt@icloud.com>
2026-09-15 20:22:04 +0200

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
